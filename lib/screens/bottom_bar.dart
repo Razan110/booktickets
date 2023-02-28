50e6b548344cc41f60a1de10e8b80c123e27eba6
hoom <hoom@MacBook-Pro-alkhas-b-Mohammed.local>
2023-02-28 22:45:39 +0300

    step-1:BottomNavigationBarItems

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,68 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOption = <Widget>[
+    const Text('Home'),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile'),
+  ];
+
+  void _onitemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My tickets'),
+      ),
+      body: Center(
+        child: _widgetOption[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onitemTapped,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: const Color.fromARGB(255, 26, 54, 67),
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: const Color(0xff526480),
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+            label: 'Home',
+          ),
+          const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: 'Search'),
+          const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: 'Tickets'),
+          const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_accounts_regular),
+              activeIcon:
+                  Icon(FluentSystemIcons.ic_fluent_person_accounts_filled),
+              label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
