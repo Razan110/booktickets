451681a7ddb617162eb8a4ac49087744062bfb2c
hoom <hoom@MacBook-Pro-alkhas-b-Mohammed.local>
2023-03-30 06:14:15 +0300

    step-4:bottomSheet

---
+++ lib/widgets/switch_bottom.dart
@@ -0,0 +1,53 @@
+import 'package:booktickets/utilities/styles/app_layout.dart';
+import 'package:booktickets/utilities/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+
+class SwitchBottom extends StatefulWidget {
+  const SwitchBottom({super.key});
+
+  @override
+  State<SwitchBottom> createState() => _SwitchBottomState();
+}
+
+class _SwitchBottomState extends State<SwitchBottom> {
+  bool status = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'Direct flights only',
+          style: Styles.headLineStyle4.copyWith(color: Colors.black),
+        ),
+        FlutterSwitch(
+          width: AppLayout.getWidth(60),
+          height: AppLayout.getHeight(30),
+          toggleSize: 20.0,
+          value: status,
+          borderRadius: 30.0,
+          padding: 2.0,
+          toggleColor: Colors.black12,
+          switchBorder: Border.all(
+            color: Colors.black,
+            width: AppLayout.getWidth(1),
+          ),
+          toggleBorder: Border.all(
+            color: Styles.contanierBg,
+            width: AppLayout.getWidth(10),
+          ),
+          activeColor: Styles.orangeColor,
+          inactiveColor: Styles.grayColor,
+          onToggle: (val) {
+            setState(
+              () {
+                status = val;
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/text_field.dart
@@ -0,0 +1,42 @@
+import 'package:booktickets/utilities/styles/app_layout.dart';
+import 'package:booktickets/utilities/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final String hintTextC;
+  final IconData icon;
+  const TextFieldContainer(
+      {Key? key, required this.hintTextC, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, left: 16),
+      child: Row(
+        children: [
+          Icon(icon),
+          Gap(AppLayout.getWidth(25)),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: hintTextC,
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  color: Styles.grayColor,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Styles.orangeColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
